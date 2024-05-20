a73249695f9c583b052f238be812b44b20295dcd
FranGalanSan <116568680+FranGalanSan@users.noreply.github.com>
2024-05-20 12:52:34 +0200

    Luces
    Añadida luces para puzle laberinto y monedas para saber cuando estan bien resuelto. ademas de alguna cosilla mas

--- Scripts/Puertas/AbrirPuertaLaberinto.cs
+++ Scripts/Puertas/AbrirPuertaLaberinto.cs
@@ -6,12 +6,19 @@ public class AbrirPuertaLaberinto : MonoBehaviour, Interactable
 {
     bool puerta = false;
     public GameObject p;
+    public Renderer l1,l2,l3;
     static int activos = 0;
+    public Material lighton;
    
    public void Interact(){
         transform.gameObject.tag = "Untagged";
         activos++;
+        if(activos==1)
+            l1.material = lighton;
+        if(activos==2)
+            l2.material = lighton;
         if(puerta==false && activos==3){
+            l3.material = lighton;
             p.SetActive(false);
             puerta=true;        
         }  
--- Scripts/Puzles/Moneda/MonedaTrigger.cs
+++ Scripts/Puzles/Moneda/MonedaTrigger.cs
@@ -9,7 +9,13 @@ public class MonedaTrigger : MonoBehaviour, Interactable
 {
     public UnityEvent interactAction;
     //public CinemachineVirtualCamera puzzleCam;
+    //private AudioSource sonido;
     public GameObject puerta;
+    public Renderer l1;
+    public Material lighton;
+    private void Start(){
+       // sonido=GetComponent<AudioSource>();
+    }
     public void Interact(){
         transform.gameObject.tag = "Untagged";
         interactAction.Invoke();  
@@ -23,6 +29,8 @@ public class MonedaTrigger : MonoBehaviour, Interactable
 
     public void Completado(){
         puerta.SetActive(false);
+        l1.material = lighton;
+        //sonido.Play();
         //puzzleCam.enabled = false;
     }
 }
